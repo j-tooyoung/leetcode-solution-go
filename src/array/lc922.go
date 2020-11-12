d58d390e35d147de58f4233066d7af095bafc650
xpfh <xupeng4215@fiberhome.com>
2020-11-12 09:00:49 +0800

    submit lc922

---
+++ src/array/lc922.go
@@ -0,0 +1,19 @@
+package array
+
+func sortArrayByParityII(A []int) []int {
+	i,j := 0, 1
+	for j < len(A) && i < len(A) {
+		for i < len(A) && A[i] % 2 == 0 {
+			i += 2
+		}
+		for j < len(A) && A[j] % 2 == 1 {
+			j += 2
+		}
+		if i < len(A) && j < len(A) {
+			A[i], A[j] = A[j], A[i]
+			i += 2
+			j += 2
+		}
+	}
+	return A
+}
---
+++ src/heap/lc937.go
@@ -0,0 +1,51 @@
+package heap
+
+import "container/heap"
+
+type pair struct {
+	dist int
+	point []int
+}
+
+type hp []pair
+
+func (h hp) Len() int {
+	return len(h)
+}
+
+func (h hp) Less(i, j int) bool {
+	return h[i].dist > h[j].dist
+}
+
+func (h hp) Swap(i, j int) {
+	h[i],h[j] = h[j] ,h[i]
+}
+
+func (h *hp) Push(x interface{}) {
+	*h =append(*h,x.(pair))
+}
+
+func (h *hp) Pop() interface{} {
+	a := *h;
+	v := a[len(a)-1]
+	*h = a[:len(a)-1]
+	return v
+}
+
+func kClosest(points [][]int, k int) (ans [][]int) {
+	h := make(hp, k)
+	for i, p := range points {
+		h[i] = pair{p[0]*p[0] + p[1]*p[1], p}
+	}
+	heap.Init(&h)	 // O(k) 初始化堆
+	for _, p := range points[k:] {
+		if dist := p[0] *p[0] + p[1] *p[1]; dist < h[0].dist {
+			h[0] = pair{dist: dist, point: p}
+			heap.Fix(&h, 0) // 效率比 pop 后 push 要快
+		}
+	}
+	for _, p := range h {
+		ans = append(ans, p.point)
+	}
+	return
+}
