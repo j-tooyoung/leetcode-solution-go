34cf705eddfcba451671778ab83fb9dfe91ce00a
xpfh <xupeng4215@fiberhome.com>
2020-11-27 09:37:52 +0800

    submit lc454

--- src/hash/lc454.go
+++ src/hash/lc454.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 // 四數相加ii
 func fourSumCount(A []int, B []int, C []int, D []int) int {
 	res := 0
 	m1 := make(map[int]int,510)
-	m2 := make(map[int]int,510)
+	//m2 := make(map[int]int,510)
 	for _, v1 := range A {
 		for _, v2 := range B {
 			m1[v1+v2]++
@@ -14,14 +12,15 @@ func fourSumCount(A []int, B []int, C []int, D []int) int {
 	}
 	for _, v1 := range C {
 		for _, v2 := range D {
-			m2[v1+v2]++
+			//m2[v1+v2]++
+			res += m1[-v1-v2]
 		}
 	}
 
-	for index := range m1 {
-		fmt.Println(index,m2[-index])
-
-		res += m1[index] * m2[-index]
-	}
+	//for index := range m1 {
+	//	fmt.Println(index,m2[-index])
+	//
+	//	res += m1[index] * m2[-index]
+	//}
 	return res
 }
